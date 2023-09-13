dd33c28e8b7aef7d5fb87f2ace81235cced5e8e1
Brian Spendolini <btspendo@gmail.com>
2023-09-13 12:29:46 -1000

    added dev container

--- .devcontainer/scripts/install-dev-tools.sh
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -5,13 +5,10 @@ sudo apt install php-dev -y
 dotnet tool install -g microsoft.sqlpackage
 sudo apt-get update
 sudo apt-get install sqlcmd
-sudo su
 ACCEPT_EULA=Y apt-get install -y --allow-downgrades msodbcsql18 odbcinst=2.3.7 odbcinst1debian2=2.3.7 unixodbc-dev=2.3.7 unixodbc=2.3.7
 pecl install sqlsrv
 pecl install pdo_sqlsrv
-apt-get install php-common
-phpenmod sqlsrv pdo_sqlsrv
-echo extension=sqlsrv.so >> /usr/local/php/8.2.0/ini/php.ini
-echo extension=pdo_sqlsrv.so >> /usr/local/php/8.2.0/ini/php.ini
-exit
-
+sudo apt-get install php-common
+sudo phpenmod -v 8.2.0 sqlsrv pdo_sqlsrv
+sudo echo extension=sqlsrv.so >> /usr/local/php/8.2.0/ini/php.ini
+sudo echo extension=pdo_sqlsrv.so >> /usr/local/php/8.2.0/ini/php.ini
