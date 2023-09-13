4fe8b3f7b3a205504b1ec82401f1bdd02b2a8c84
Brian Spendolini <btspendo@gmail.com>
2023-09-13 11:18:00 -1000

    added dev container

--- .devcontainer/scripts/install-dev-tools.sh
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -1,6 +1,10 @@
 sudo cp ./scripts/ms-repo.pref /etc/apt/preferences.d/
 sudo apt-get update
 sudo apt install dotnet-sdk-6.0 -y
+sudo apt install php-dev -y
 dotnet tool install -g microsoft.sqlpackage
 sudo apt-get update
 sudo apt-get install sqlcmd
+sudo apt-get 
+pecl install sqlsrv-5.11.1
+pecl install pdo_sqlsrv-5.11.1
